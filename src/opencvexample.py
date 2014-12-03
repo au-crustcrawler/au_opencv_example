dc2bfb678a2e77ad80a447c0ff7e2edc4e1700ce
Morten Larsen <mortenlarsens@gmail.com>
2014-12-03 10:11:19 +0100

    fixed buffer size

--- src/opencvexample.py
+++ src/opencvexample.py
@@ -10,14 +10,17 @@ def get_from_webcam():
     """
     stream=urllib.urlopen('http://192.168.0.20/image/jpeg.cgi')
     bytes=''
-    bytes+=stream.read(32384)
+    bytes+=stream.read(64500)
     a = bytes.find('\xff\xd8')
     b = bytes.find('\xff\xd9')
+
     if a != -1 and b != -1:
         jpg = bytes[a:b+2]
         bytes= bytes[b+2:]
         i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR)
         return i
+    else:
+        print "did not receive image, try increasing the buffer size in line 13:"
 
 def get_from_file(filename):
     """
